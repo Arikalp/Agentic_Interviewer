459e380e27dbf0907bc43b4c36ccab2a38fc3bd0
Arikalp <sankalpkrsaini@gmail.com>
2026-04-15 17:15:18 +0530

    Added the vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Analytics } from '@vercel/analytics/next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ClerkProvider>
           {children}
+          <Analytics />
         </ClerkProvider>
       </body>
     </html>
